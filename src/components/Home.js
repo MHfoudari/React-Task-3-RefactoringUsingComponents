7bb6569a567416b7bc9c30f2fb2ee2efa73db076
MHfoudari <eng.mohammed.alfoudari@gmail.com>
2023-06-17 18:31:51 +0300

    fininshed the first part of the task

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import products from "../products";
+const Home = () => {
+  const productList = products.map((product) => (
+    <div className="product">
+      <img className="productImage" alt={product.name} src={product.image} />
+      <p className="text">{product.name}</p>
+      <p className="text">{product.price} KD</p>
+    </div>
+  ));
+  return (
+    <div className="body">
+      <div>
+        <h1 className="text">Cookies and Beyond</h1>
+        <h4 className="text">Where cookie maniacs gather</h4>
+        <img
+          alt="cookie shop"
+          src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg"
+          className="shopImage"
+        />
+      </div>
+      <div className="list">{productList}</div>
+    </div>
+  );
+};
+
+export default Home;
